21999e845e7c6c33e8292e6d86b61331f12d6d8f
rondobson <112710619+rondobson@users.noreply.github.com>
2023-10-09 02:40:46 -0400

    added signup for organizations and fixed small bug

--- signup.php
+++ signup.php
@@ -7,10 +7,17 @@ require 'connect.php'; // Connecting to database
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $userType = $_POST['$userType'];
+    $userType = $_POST['userType'];
 
     if ($userType === 'volunteer'){
         $sql = "INSERT INTO volunteers (name, password) VALUES ('$name' , '$password')";
+        if($conn->query($sql) === TRUE){ // Checks if data was inserted into database by checking if the mysqli_result object is true
+            echo "New record created successfully";
+        } else {
+            echo "Error";
+         }
+    } else if ($userType === 'organization'){
+        $sql = "INSERT INTO organizations (name, password) VALUES ('$name' , '$password')";
         if($conn->query($sql) === TRUE){ // Checks if data was inserted into database by checking if the mysqli_result object is true
             echo "New record created successfully";
         } else {
