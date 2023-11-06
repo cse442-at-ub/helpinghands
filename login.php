03bedbffe596512049d2a52b5bcbdc941efb6a52
rondobson <112710619+rondobson@users.noreply.github.com>
2023-11-06 02:59:01 -0500

    Added redirect upon login, and event overpopulation prevention

--- login.php
+++ login.php
@@ -4,6 +4,11 @@ require 'connect.php'; // Connecting to database
 
 session_start(); // Creating session
 
+if(isset($_SESSION['message'])){
+    consoleLog($_SESSION['message']);
+    unset($_SESSION['message']);
+}
+
 // Check if email and password are given using the POST method
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
@@ -28,12 +33,18 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
                     // Storing username and userType in session
                     $_SESSION['email'] = $email;
                     $_SESSION['userType'] = $userType;
-                    echo $_SESSION['userType'] . " " . $_SESSION['email'] . " logged in successfully";
+                    //echo $_SESSION['userType'] . " " . $_SESSION['email'] . " logged in successfully";
+                    $_SESSION['message'] = $_SESSION['userType'] . " " . $_SESSION['email'] . " logged in succesfully"; // setting the console output as a message
+                    header("Location: homepage.php");
+                    exit();
+                    
+                } else { // If password is incorrect
+                    $_SESSION['message'] = "Incorrect email or password";
+                    header("Locaiton: signin.html");
+                    exit();
                 }
-        }
-
-        else{ // If the credentials arent in the table
-            echo "Incorrect email or password";
+        } else{ // If the credentials arent in the table
+             consoleLog("Incorrect email or password");
         }
 /*
     $stmt->close();
@@ -43,5 +54,9 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
 // session_destroy(); // destroys session and clears all session data
 
+
+function consoleLog($data){
+    echo "<script>console.log('$data');</script>";
+}
 
 ?>
