6d5de04bdbef1cad3231d5c8bc4ccedba5245bfc
unnati <unnatipadvaya@gmail.com>
2023-11-06 05:32:04 -0500

    create events adds in events

--- organisationprofilepage.php
+++ organisationprofilepage.php
@@ -1,3 +1,16 @@
+<?php
+require('connect.php');
+session_start();
+if (!isset($_SESSION['email']) || $_SESSION['userType'] != 'organization') {
+  header('location: signin.html');
+}
+$user = $_SESSION['email'];
+$sql = "SELECT * FROM `events` WHERE `username`=?";
+$events = $conn->prepare($sql);
+$events->bind_param('s', $user);
+$events->execute();
+$events = $events->get_result();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -65,24 +78,27 @@
              <img class="hideall d_none" src="Images/png-transparent-arrow-expand-expand-less-expandless-top-up-navigation-set-arrows-part-one-icon.png" onclick="showall(this)" data-box="events">
                 <button class="showall" onclick="hideall(this)" data-box="events">Hide All</button>
             
-    </div>
+                </div>
     <div class="second_box " id="events">
-        <div class="logo_box">
-          <img src="Images/HomeAid-National.png">
+      <?php while ($event = $events->fetch_assoc()) { ?>
 
-            <div class="df">
-              <div class="box_rating">
-                4.92
-              </div>
-              <div class="pepople_rating">
-                <img src="Images/people.png">
-                30/50
-              </div>
+        <div class="logo_box mt-2">
+          <img src="<?php echo $event['image']; ?>">
+
+          <div class="df">
+            <div class="box_rating">
+              4.92
             </div>
-            <div class="date">
-            <span>Apr 08, 2023 - Aug 03, 2023</span><span>8am - 1pm</span>
+            <div class="pepople_rating">
+              <img src="Images/people.png">
+              0/<?php echo $event['volunteersRequired']; ?>
             </div>
+          </div>
+          <div class="date">
+            <span><?php echo date('M d, Y', strtotime($event['startDate'])); ?> - <?php echo date('M d, Y', strtotime($event['endDate'])); ?></span><span><?php echo date('ha', strtotime($event['startTime'])); ?> - <?php echo date('ha', strtotime($event['endTime'])); ?></span>
+          </div>
         </div>
+      <?php } ?>
     </div>
 
 
