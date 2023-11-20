04f1ffc0ee849b3c820ea5f6009fd19bc0bf99f2
malandior <90802477+malandior@users.noreply.github.com>
2023-11-20 01:50:24 -0500

    bookmarkEvent.php fix

--- bookmarkEvent.php
+++ bookmarkEvent.php
@@ -9,28 +9,55 @@ require 'connect.php'; // Connecting to database
 if (isset($_POST['user']) && isset($_POST['eventID'])) {
     $user = $_POST["user"];
     $eventID = $_POST["eventID"];
-    
-    try {
-        // Check if the user is already registered
-        $stmt = $conn->prepare("SELECT * FROM bookmarkedEvents WHERE user = ? AND eventID = ?");
-        $stmt->bind_param("si", $user, $eventID);
-        $stmt->execute();
-        $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            echo "You are already bookmarked this event.";
-            exit;
-        }
+    // Check if the user is already registered
+    $stmt = $conn->prepare("SELECT * FROM bookmarkedEvents WHERE user = ? AND eventID = ?");
+    $stmt->bind_param("si", $user, $eventID);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        // Bookmark the event for the user
-        $stmt = $conn->prepare("INSERT INTO bookmarkedEvents (user, eventID) VALUES (? , ?)");
-        $stmt->execute([$user, $eventID]);
-
-        echo "You have successfully registered for the event.";
-
-    } catch(PDOException $error) {
-        echo "Error: " . $error->getMessage(); // echos error message
+    if ($result->num_rows > 0) {
+        echo "You already bookmarked this event.";
+        exit;
     }
+/*
+    // Check if the event has room for the new volunteer
+
+    // Getting the current number of registered volunteers
+    $stmt = $conn->prepare("SELECT * FROM eventRegistrations WHERE eventID = ?");
+    $stmt->bind_param("i", $eventID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $numRegistrations = $result->num_rows; // Stores the current number of registrations for the event
+
+    // Getting the max number of volunteers
+    $stmt = $conn->prepare("SELECT volunteersRequired FROM events WHERE eventID = ?");
+    $stmt->bind_param("i", $eventID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $maxVolunteersRow = $result->fetch_assoc();
+    $maxVolunteers = $maxVolunteersRow['volunteersRequired'];
+
+    // Comparing
+    if ($numRegistrations >= $maxVolunteers) {
+        echo "Unfortunately, the maxinmum number of volunteers has been reached for this event";
+        exit;
+    }
+*/
+
+
+    // Register the user for the event
+    $stmt = $conn->prepare("INSERT INTO bookmarkedEvents (user, eventID) VALUES (? , ?)"); // Preparing SQL statement to prevent SQL injection
+    $stmt->bind_param("si", $user, $eventID);
+
+    // Error handling
+    if(!$stmt->execute()){
+        echo "Error: " . $stmt->error; // echoes error message
+        exit;
+    }
+
+    echo "You have successfully bookmarked the event.";
+
 }
 
 ?>
