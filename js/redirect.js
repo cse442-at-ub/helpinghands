e1b74d87fa8ac69a528b01ce9c9313c4c84d193f
malandior <90802477+malandior@users.noreply.github.com>
2023-11-27 01:03:59 -0500

    Fix for organization not having correct url

--- js/redirect.js
+++ js/redirect.js
@@ -3,7 +3,7 @@ function redirectToPage(role,uid) {
     if (role === 'volunteer') {
         window.location.href = 'volunteerprofilepage.php?uid=' + uid.toString();
     } else if (role === 'organization') {
-        window.location.href = 'organisationprofilepage.php' + uid.toString();
+        window.location.href = 'organisationprofilepage.php?uid=' + uid.toString();
     } else {
         // Default redirection if the role doesn't match any specified roles
         window.location.href = 'homepage.php';
